ef8c72d5210e9f0dce1bd517f595f9e308cfbbe8
AliMoradzadeh <moracode.master@gmail.com>
2023-06-17 03:23:27 +0330

    add generating pdf feature for orders with weasyprint (need execute "collectstaic" command with manage.py)

--- orders/admin.py
+++ orders/admin.py
@@ -42,6 +42,10 @@ def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
@@ -50,7 +54,7 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', order_payment, 'created', 'updated', order_detail]
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', order_payment, 'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
--- orders/tasks.py
+++ orders/tasks.py
@@ -9,7 +9,4 @@ def order_created(order_id):
     subject = f'Order nr. {order.id}'
     message = f'Dear {order.first_name},\n\n' + f'You have successfully placed an order. Your order ID is {order.id}.'
     mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
-    print(32* "*")
-    print(mail_sent)
-    print(32* "*")
     return mail_sent
