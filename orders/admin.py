08909af5b4020674285b1cc629ed7da7ef1b6e23
AliMoradzadeh <moracode.master@gmail.com>
2023-06-17 03:10:12 +0330

    add view link fir order detail in admin panel og order.Orders instances

--- orders/admin.py
+++ orders/admin.py
@@ -4,6 +4,7 @@ from .models import Order, OrderItem
 import csv
 import datetime
 from django.http import HttpResponse
+from django.urls import reverse
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -37,6 +38,11 @@ def order_payment(obj):
     return ''
 order_payment.short_description = 'Stripe payment'
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -44,7 +50,7 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', order_payment, 'created', 'updated']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', order_payment, 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
